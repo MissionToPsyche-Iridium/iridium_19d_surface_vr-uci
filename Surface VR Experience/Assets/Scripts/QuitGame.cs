fc48a1a0230763a4f1470b725bcf02106c3c4487
Dylan Peppard <122654442+DylanPeppard@users.noreply.github.com>
2025-01-24 13:16:14 -0800

    "Mode" and "Quit" confirmation panels
    MainMenu:
    -Pressing "QUIT" brings up panel asking to confirm
    -Pressing "MODE" brings up mode selection screen
    -Pressing "PLAY" or "EVENT" asks for confirmation

--- Surface VR Experience/Assets/Scripts/QuitGame.cs
+++ Surface VR Experience/Assets/Scripts/QuitGame.cs
@@ -2,9 +2,24 @@ using UnityEngine;
 
 public class QuitGame : MonoBehaviour
 {
+    [SerializeField] GameObject quitCanvas;
+
     public void ExitGame()
     {
         Application.Quit();
         Debug.Log("Game has been quit.");
+    }
+
+    public void confirmIfQuitting()
+    {
+        if (quitCanvas != null)
+        {
+            quitCanvas.SetActive(true);
+        }
+    }
+
+    public void cancelQuitting()
+    {
+        quitCanvas.SetActive(false);
     }
 }
