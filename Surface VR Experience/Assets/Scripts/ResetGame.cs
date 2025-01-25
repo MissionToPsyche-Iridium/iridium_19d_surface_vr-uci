e2034d7c897794f57042bbd083a916d6802dfb62
Dylan Peppard <122654442+DylanPeppard@users.noreply.github.com>
2025-01-24 16:49:36 -0800

    Fixed reload bug, in-game UI update
    -Reloading in play mode no longer crashes game
    -In-game menu updated to match figma in HCL scene

--- Surface VR Experience/Assets/Scripts/ResetGame.cs
+++ Surface VR Experience/Assets/Scripts/ResetGame.cs
@@ -7,10 +7,18 @@ public class ResetGame : MonoBehaviour
 {
     public void ReloadScene() 
     {
-        // Stores the remaining time in GameState and then reloads the scene
-        Timer timer  = FindObjectOfType<Timer>();
-        GameState.timeBeforeReset = timer.remainingTime;
-        Scene currentScene = SceneManager.GetActiveScene();
-        SceneManager.LoadScene(currentScene.name);
+        if (GameState.isEventMode)
+        {
+            // Stores the remaining time in GameState and then reloads the scene
+            Timer timer  = FindObjectOfType<Timer>();
+            GameState.timeBeforeReset = timer.remainingTime;
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+        }
+        else
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+        }
     }
 }
